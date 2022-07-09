88bc507fa3e8faddafc3134c0afd8da14c2d91c4
hong-kyungs <hkyungs11@naver.com>
2022-07-09 19:41:55 +0900

    js/scss 생성$연결

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import { Route } from 'react-router-dom';
+//common
+import Header from './components/common/Header';
+import Footer from './components/common/Footer';
+
+//main
+import Visual from './components/main/Visual';
+import Content from './components/main/Content';
+
+//sub
+import Department from './components/sub/Department';
+import Youtube from './components/sub/Youtube';
+import Gallery from './components/sub/Gallery';
+import Community from './components/sub/Community';
+import Location from './components/sub/Location';
+import Members from './components/sub/Members';
+
+import './scss/style.scss';
+
+function App() {
+	return (
+		<>
+			<Header />
+
+			<Route exact path='/'>
+				<Visual />
+				<Content />
+			</Route>
+
+			<Route path='/department' component={Department} />
+
+			<Route path='/youtube' component={Youtube} />
+
+			<Route path='/gallery' component={Gallery} />
+
+			<Route path='/community' component={Community} />
+
+			<Route path='/location' component={Location} />
+
+			<Route path='/members' component={Members} />
+
+			<Footer />
+		</>
+	);
+}
+
+export default App;
