d07caedc9c1103f828e37697f0232bbdf7dc2239
hong-kyungs <hkyungs11@naver.com>
2022-07-11 04:33:02 +0900

    department page - intro_container 추가

--- src/components/sub/Department.js
+++ src/components/sub/Department.js
@@ -25,37 +25,68 @@ function Department() {
 
 	return (
 		<Layout name={'Department'}>
-			<p>Easy and Simple</p>
-			<h2>Meet the Team</h2>
-			{Members.map((member, idx) => {
-				return (
-					<article key={idx}>
-						<div className='inner'>
-							<div className='pic'>
-								<img src={`${path}/img/${member.pic}`} alt={member.name} />
+			<div className='intro_container'>
+				<div className='wrap'>
+					<div className='item'>
+						<span>01</span>
+						<h3>User Experience</h3>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis,
+							numquam?
+						</p>
+					</div>
+					<div className='item'>
+						<span>02</span>
+						<h3>Graphic Design</h3>
+						<p>
+							Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia,
+							rem.
+						</p>
+					</div>
+					<div className='item'>
+						<span>03</span>
+						<h3>Develop System</h3>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+							Perferendis, in!
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<div className='member_intro'>
+				<p>Easy and Simple</p>
+				<h2>Meet the Team</h2>
+				{Members.map((member, idx) => {
+					return (
+						<article key={idx}>
+							<div className='inner'>
+								<div className='pic'>
+									<img src={`${path}/img/${member.pic}`} alt={member.name} />
+								</div>
+								<div className='txt'>
+									<h3>{member.name}</h3>
+									<p>{member.position}</p>
+								</div>
+								<div className='sns'>
+									<a href='#'>
+										<FontAwesomeIcon icon={faFacebookF} className='i' />
+									</a>
+									<a href='#'>
+										<FontAwesomeIcon icon={faTwitter} className='i' />
+									</a>
+									<a href='#'>
+										<FontAwesomeIcon icon={faInstagram} className='i' />
+									</a>
+									<a href='#'>
+										<FontAwesomeIcon icon={faEnvelope} className='i' />
+									</a>
+								</div>
 							</div>
-							<div className='txt'>
-								<h3>{member.name}</h3>
-								<p>{member.position}</p>
-							</div>
-							<div className='sns'>
-								<a href='#'>
-									<FontAwesomeIcon icon={faFacebookF} className='i' />
-								</a>
-								<a href='#'>
-									<FontAwesomeIcon icon={faTwitter} className='i' />
-								</a>
-								<a href='#'>
-									<FontAwesomeIcon icon={faInstagram} className='i' />
-								</a>
-								<a href='#'>
-									<FontAwesomeIcon icon={faEnvelope} className='i' />
-								</a>
-							</div>
-						</div>
-					</article>
-				);
-			})}
+						</article>
+					);
+				})}
+			</div>
 		</Layout>
 	);
 }
