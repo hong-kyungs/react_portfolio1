cf6dba16b56e9e6a9b74dc60ebe450b987177b89
hong-kyungs <hkyungs11@naver.com>
2022-07-15 04:21:11 +0900

    회원가입 페이지 진행중

--- src/components/sub/Members.js
+++ src/components/sub/Members.js
@@ -4,6 +4,12 @@ import { useEffect, useState } from 'react';
 function Members() {
 	const initVal = {
 		userid: '',
+		pwd1: '',
+		pwd2: '',
+		email: '',
+		gender: null,
+		preference: null,
+		store: '',
 	};
 	const [Val, setVal] = useState(initVal);
 
@@ -11,8 +17,33 @@ function Members() {
 
 	const check = (value) => {
 		const errs = {};
+		const eng = /[a-zA-Z]/;
+		const num = /[0-9]/;
+		const spc = /[~!@#$%^&*()_+?><]/;
+
 		if (value.userid.length < 5) {
-			errs.userid = '아이디를 5글자 이상 입력하세요.';
+			errs.userid = 'Please enter at least 5 characters.';
+		}
+		if (
+			value.pwd1 < 5 ||
+			!eng.test(value.pwd1) ||
+			!num.test(value.pwd1) ||
+			!spc.test(value.pwd1)
+		) {
+			errs.pwd1 =
+				'The password entered is invalid. (Minimum 5 characters long incluing alphabetic, numetic and special character.';
+		}
+		if (value.pwd1 !== value.pwd2 || value.pwd2 < 5) {
+			errs.pwd2 = 'The confirm password does not match with the password.';
+		}
+		if (value.email.length < 8 || !/@/.text(value.email)) {
+			errs.email = 'Please enter at least 8 characters long including @.';
+		}
+		if (!Val.gender) {
+			errs.gender = 'Please choose your gender.';
+		}
+		if (!Val.preference) {
+			errs.preference = 'Please choose your preference.';
 		}
 		return errs;
 	};
@@ -25,6 +56,21 @@ function Members() {
 		e.preventDefault();
 		setErr(check(Val));
 	};
+	const handleRadio = (e) => {
+		const { name } = e.target;
+		const isCheck = e.target.checked;
+		setVal({ ...Val, [name]: isCheck });
+	};
+	const handleCheck = (e) => {
+		let isCheck = false;
+		const { name } = e.target;
+		const inputs = e.target.parentElement.querySelectorAll('input');
+		inputs.forEach((el) => {
+			if (el.checked) isCheck = true;
+		});
+		setVal({ ...Val, [name]: isCheck });
+	};
+	const handleSelect = () => {};
 
 	useEffect(() => {
 		console.log(Err);
@@ -36,6 +82,7 @@ function Members() {
 				<form onSubmit={handleSubmit}>
 					<fieldset>
 						<legend className='h'>회원가입</legend>
+						{/* userID */}
 						<div className='field'>
 							<input
 								type='text'
@@ -45,9 +92,113 @@ function Members() {
 								value={Val.userid}
 								onChange={handleChange}
 							/>
-							<label htmlFor='userid'>USER ID*</label>
+							<label htmlFor='userid'>USER ID *</label>
 							<span className='err'>{Err.userid}</span>
 						</div>
+
+						{/* password */}
+						<div className='field'>
+							<input
+								type='password'
+								name='pwd1'
+								id='pwd1'
+								placeholder=' '
+								value={Val.pwd1}
+								onChange={handleChange}
+							/>
+							<label htmlFor='pwd1'>PASSWORD *</label>
+							<span className='err'>{Err.pwd1}</span>
+						</div>
+						<div className='field'>
+							<input
+								type='password'
+								name='pwd2'
+								id='pwd2'
+								placeholder=' '
+								value={Val.pwd2}
+								onChange={handleChange}
+							/>
+							<label htmlFor='pwd2'>CONFIRM PASSWORD *</label>
+							<span className='err'>{Err.pwd2}</span>
+						</div>
+						{/* email */}
+						<div className='field'>
+							<input
+								type='text'
+								name='email'
+								id='email'
+								placeholder=' '
+								value={Val.email}
+								onChange={handleChange}
+							/>
+							<label htmlFor='email'>EMAIL *</label>
+							<span className='err'>{Err.email}</span>
+						</div>
+
+						<div className='field2'>
+							<div className='radio'>
+								<p>GENDER *</p>
+								<label htmlFor='male'>MALE</label>
+								<input
+									type='radio'
+									id='male'
+									name='gender'
+									onChange={handleRadio}
+								/>
+								<label htmlFor='female'>FEMALE</label>
+								<input
+									type='radio'
+									id='female'
+									name='gender'
+									onChange={handleRadio}
+								/>
+							</div>
+							<span className='err'>{Err.gender}</span>
+						</div>
+						{/* preference */}
+						<div className='field2'>
+							<div className='checkbox'>
+								<p>PREFERENCE *</p>
+								<label htmlFor='speaker'>SPEAKER</label>
+								<input
+									type='checkbox'
+									id='speaker'
+									name='preference'
+									onChange={handleCheck}
+								/>
+
+								<label htmlFor='headphone'>HEADPHONE</label>
+								<input
+									type='checkbox'
+									id='headphone'
+									name='preference'
+									onChange={handleCheck}
+								/>
+
+								<label htmlFor='tv'>TV</label>
+								<input
+									type='checkbox'
+									id='TV'
+									name='preference'
+									onChange={handleCheck}
+								/>
+							</div>
+							<span className='err'>{Err.preference}</span>
+						</div>
+
+						<div className='field3'>
+							<div className='select'>
+								<label htmlFor='store'>NEAREST *</label>
+								<select name='store' id='store' onChange={handleSelect}>
+									<option value=''>Nearest Store</option>
+									<option value='hanam'>Hanam</option>
+									<option value='seuol'>Seoul</option>
+									<option value='busan'>Busan</option>
+								</select>
+								<span className='err'>{Err.store}</span>
+							</div>
+						</div>
+
 						<div className='btnset'>
 							<input type='reset' value='CANCEL' />
 							<input type='submit' value='SEND' />
