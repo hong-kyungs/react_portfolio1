1bd38cac060d3be421b4ce1ab8fb651eac36d0af
hong-kyungs <hkyungs11@naver.com>
2022-07-27 02:18:53 +0900

    헤더호버효과 & 갤러리 타이포추가

--- src/components/main/Visual.js
+++ src/components/main/Visual.js
@@ -80,30 +80,82 @@ function Visual() {
 						src={process.env.PUBLIC_URL + '/img/Main1.jpg'}
 						alt='메인사진1'
 					/>
+					<div className='visual_txt'>
+						<span>HEAR IT. SEE IT. LIVE IT.</span>
+						<h2>The show starts here</h2>
+						<p>
+							Let the music pull you in. Let the action take you away.
+							Discovering new levels of depth, detail and delight is what
+							elevates every moment. And it's what drives us to create sound and
+							vision experiences that go beyond expectation. So that every time
+							you press play, you feel something new – right from the comfort of
+							home.
+						</p>
+					</div>
 				</li>
 				<li>
 					<img
 						src={process.env.PUBLIC_URL + '/img/Main2.jpg'}
 						alt='메인사진2'
 					/>
+					<div className='visual_txt'>
+						<span>CONNECTED SPEAKERS</span>
+						<h2>Speakers, simply connected</h2>
+						<p>
+							No cables, no fuss. Connect speakers quickly and easily through
+							WiFi, and be connected everywhere. Control the whole experience
+							from the palm of your hand: play what you want, the way you want.
+						</p>
+					</div>
 				</li>
 				<li>
 					<img
 						src={process.env.PUBLIC_URL + '/img/Main3.jpg'}
 						alt='메인사진3'
 					/>
+					<div className='visual_txt'>
+						<span>HOME THEATRE SYSTEMS</span>
+						<h2>Remastering the home cinema experience</h2>
+						<p>
+							Discover the thrill of upgrading a Beovision TV with our stunning
+							speakers and creating your own home cinema experience. Your
+							possibilities are endless, and the experience is guaranteed to
+							bring a smile on your face.
+						</p>
+					</div>
 				</li>
 				<li>
 					<img
 						src={process.env.PUBLIC_URL + '/img/Main4.jpg'}
 						alt='메인사진4'
 					/>
+					<div className='visual_txt'>
+						<span>MAKE WORKING FROM HOME A PLEASURE</span>
+						<h2>Work from home</h2>
+						<p>
+							Working from home has moved from an occasional occurrence to an
+							everyday expectation. Embrace the potential for flexibility and
+							reduce challenges by creating a home office space that supports
+							and nourishes you.
+						</p>
+					</div>
 				</li>
 				<li>
 					<img
 						src={process.env.PUBLIC_URL + '/img/Main5.jpg'}
 						alt='메인사진5'
 					/>
+					<div className='visual_txt'>
+						<span>BANG & OLUFSEN</span>
+						<h2>Our journey into gaming</h2>
+						<p>
+							Bang & Olufsen engineers are always searching for the next way to
+							enhance listening experiences and in recent years, the world of
+							gaming has presented itself as an increasingly compelling pastime.
+							Gaming can be as immersive as watching a movie with surround sound
+							– with the right headphones.
+						</p>
+					</div>
 				</li>
 			</ul>
 
